bc485a5c57a420abffbafda24abab3a4d3330ff8
Alvaro Meneses <alvaro.mateo2504@gmail.com>
2025-07-06 02:43:35 +0000

    Primer commit: solución del juego Rock Paper Scissors

--- RPS.py
+++ RPS.py
@@ -1,10 +1,53 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+# RPS.py
 
-def player(prev_play, opponent_history=[]):
+import random
+from collections import Counter
+
+def player(prev_play, opponent_history=[], memory={}):
+    # Reiniciar memoria en nueva partida
+    if not opponent_history:
+        memory.clear()
+        memory['moves'] = ['R', 'P', 'S']
+        memory['freq'] = {'R': 0, 'P': 0, 'S': 0}
+        memory['last_move'] = ''
+        memory['counter'] = {'R': 'P', 'P': 'S', 'S': 'R'}
+        memory['pattern_len'] = 3
+        memory['play_order'] = {}
+        memory['game_count'] = 0
+
     opponent_history.append(prev_play)
+    memory['game_count'] += 1
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    # Actualizar frecuencias
+    if prev_play:
+        memory['freq'][prev_play] += 1
 
+    # --- Predicción basada en patrones ---
+    def predict_pattern(history, pattern_len=3):
+        play_order = memory.get('play_order', {})
+        if len(history) < pattern_len + 1:
+            return random.choice(memory['moves'])
+
+        last_pattern = ''.join(history[-pattern_len:])
+        play_order[last_pattern] = play_order.get(last_pattern, 0) + 1
+        memory['play_order'] = play_order
+
+        potential_plays = [k for k in play_order if k.startswith(last_pattern[1:])]
+
+        if potential_plays:
+            next_move = max(potential_plays, key=lambda x: play_order[x])[-1]
+            return memory['counter'][next_move]
+        return random.choice(memory['moves'])
+
+    # --- Jugada principal ---
+    if memory['game_count'] < 5:
+        guess = random.choice(memory['moves'])  # Aleatorio al inicio
+    else:
+        freq_move = max(memory['freq'], key=memory['freq'].get)
+        guess = memory['counter'][freq_move]  # Contra movida más común
+        pattern_guess = predict_pattern(opponent_history)
+        guess = pattern_guess  # Usamos predicción de patrones
+
+    memory['last_move'] = guess
     return guess
+    
